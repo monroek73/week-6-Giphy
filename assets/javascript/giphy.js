c97a6c4a6df1edad19f8730aa34ce65d827de19d
Kim <monroek73@yahoo.com>
2016-11-05 11:47:59 -0700

    js https

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -8,7 +8,7 @@ var subject =["Aquaman", "Batman", "Captin America", "Iron Man", "Supergirl","Su
 function displayGiphy(){
 	console.log("working");
 		var hero = $(this).attr('data-name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=dc6zaTOxFJmzC&limit=10";
 	console.log(queryURL); 
     $.ajax({url: queryURL, method: 'GET'})
     .done(function(response) {
